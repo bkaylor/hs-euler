9577872b7d602e0d120e87f2d84847373cd06344
Bryan Kaylor <bsk102020@utdallas.edu>
2017-05-17 19:16:03 -0500

    Fast problem 3!

--- euler003.hs
+++ euler003.hs
@@ -1,15 +1,17 @@
 -- Largest prime factor
-isPrime :: Int -> Bool
-isPrime x = null [a | a <- [2..isqrt x], x `mod` a == 0]
-
 isqrt :: Int -> Int
 isqrt = floor . sqrt . fromIntegral
 
-primeFactorList :: Int -> [Int]
-primeFactorList x = [a | a <- [2..isqrt x], isPrime a, x `mod` a == 0] 
+isPrime :: Int -> Bool
+isPrime x = null [a | a <- [2..isqrt x], x `mod` a == 0]
 
-lpf :: Int -> Int
-lpf x = maximum [a | a <- primeFactorList x]
+firstPrimeFactor :: Int -> Int
+firstPrimeFactor x = head [a | a <- [2..isqrt x], x `mod` a == 0, isPrime a]
+
+largestPrimeFactor :: Int -> Int
+largestPrimeFactor x
+    | isPrime x = x
+    | otherwise = largestPrimeFactor (x `div` firstPrimeFactor x)
 
 euler3 :: Int -> Int
-euler3 x = lpf x
+euler3 x = largestPrimeFactor x
